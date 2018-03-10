3e08182a647efadcf65f91dd461eef3f745443f4
ozielalves <ozielalves@ufrn.edu.br>
2018-03-10 16:40:58 -0300

    Seuential search is done

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -1,1 +1,18 @@
 #include "sequential.h"
+
+int * ssearch( const int *first , const int *last , const int value )
+{
+
+        for( auto i(0) = first ; i <= last ; i++ )
+        {
+                if ( *i == value )
+                {
+                        return 1;
+                {
+        }
+
+
+        return last; // STUB
+
+}
+
