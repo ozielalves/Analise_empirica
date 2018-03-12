d3cf49382f1f5cfdf71f6e013033b38e66fc998d
Felipe Ramos <felipe.da.costa_10@hotmail.com>
2018-03-12 11:43:09 -0300

    Updates on argc, argv

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,19 @@
 #include <iostream>
 #include <algorithm>
 #include <iterator>
+#include <sstream>
 
-int main(void) {
-	int tam = 100;
+
+int main(int argc, char **argv) {
+	int tam = 20;
+	for(int i = 0; i < argc; i++){
+		std::cout << argv[i] << std::endl;
+	}
+	if( argc > 0 )
+	{
+		std::string parametro = (std::string) argv[1];
+		tam = std::atoi(parametro.c_str());
+	} 
 	int random_vector[] = {1, 42, 8, 7, 12, 19, 22, 10, 76, 1022, 3, 2, 17, 34, 64, 50};
 	long int *big_random_vector = criarVetor(tam);
 
@@ -16,9 +26,9 @@ int main(void) {
 	
 	std::cout << "Original Random Array" << std::endl;
 	// não funciona (?) // Perguntar a selan
-	//print_array( std::begin(big_random_vector), std::end(big_random_vector) );
+	//print_larray( std::begin(big_random_vector), std::end(big_random_vector) );
 	
-	print_larray( big_random_vector, big_random_vector+tam-1 );
+	print_larray( big_random_vector, big_random_vector+tam );
 	
 	return 0;
 }
