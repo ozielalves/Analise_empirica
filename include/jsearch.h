a0e4c43abf8f5eadf08f8b4b7f91264cc2b80b0c
Felipe Ramos <felipecramos@ufrn.edu.br>
2018-03-14 12:48:30 -0300

    Jump search implemented

--- include/jsearch.h
+++ include/jsearch.h
@@ -1,4 +1,9 @@
 #ifndef JSEARCH_H
 #define JSEARCH_H
 
+#include "sequential.h"
+#include <cmath>
+
+long int *jsearch( long int *first, long int *last, long int value, long int *counter );
+
 #endif
--- src/jsearch.cpp
+++ src/jsearch.cpp
@@ -1,36 +1,27 @@
 #include "jsearch.h"
-#include "sequential.h"
-#include <cmath>
 
 long int *jsearch( long int *first, long int *last, long int value, long int *counter ) 
 {
-// vector size  
-   long int n = ( last - first );
-   long int m = sqrt(n);
-   long int *last_backup;
-   last_backup = last;
+	// vector size  
+	long int n = last - first;
+	long int m = sqrt(n);
+	long int *last_backup = last;
 	
-   for( auto k(1); k*m < n; k++)
-   {
-	*counter++;
-       if( value == *(first + k*m) )
-       {	
-		   return (first + k*m);
-       }
-       if( *(first + k*m) > value )
-       {
-	// ponteiros passados - analisar
-	auto result = ssearch( (first + (k-1)*m), ((first + k*m)), value);
-		if(result != ((first + k*m)))
-		{
-			 return result;
-		}
-		else return last;
-       }
-	if( *(first + k*m) < value )
+	for( auto k(1); (k * m) < n; k++ )
 	{
-		continue;
+		*counter++;
+		if( value == *(first + k * m) )
+			return first + k * m;
+		if( *(first + k * m) > value )
+		{
+			// ponteiros passados - analisar
+			auto result = ssearch( (first + (k - 1) * m), ((first + k * m)), value);
+		
+			if( result != (first + k * m) )
+				return result;
+			else
+				return last;
+		}
 	}
-   }
-return last_backup;
+	return last_backup;
 }
