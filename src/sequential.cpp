71c044c5680be1ada32fdb83622b7f9040c7dc6e
Felipe Ramos <felipe.da.costa_10@hotmail.com>
2018-03-14 02:44:42 -0300

    Fixed infite loop

--- src/sequential.cpp
+++ src/sequential.cpp
@@ -4,11 +4,7 @@ long int *ssearch( long int *first, long int *last, long int value )
 {
 	for( long int *i = first; i < last; i++ ){
 		if ( *i == value )
-		{
-			std::cout << "Elemento " << value << " encontrado, posição " << std::hex << i << std::endl;
 			return i;
-		}
 	}
-	std::cout << "Não conseguimos encontrar o elemento!" << std::endl;
 	return last; // STUB
 }
