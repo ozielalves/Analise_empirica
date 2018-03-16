f60113f4c4cfed698d115dd6dc634d2cc13e1a8c
ozielalves <ozielalves@ufrn.edu.br>
2018-03-15 22:25:45 -0300

    Working on Ternary search

--- include/ternary.h
+++ include/ternary.h
@@ -1,5 +1,11 @@
 #ifndef TERNARY_H
 #define TERNARY_H
 
+//recursive ternary search
+long int *r_ternary( long int *first, long int *last, long int value, long int *counter );
+
+//inductive ternary search
+//long int *i_ternary( long int *first, long int *last, long int value, long int *counter );
+
 
 #endif
--- src/ternary.cpp
+++ src/ternary.cpp
@@ -1,1 +1,39 @@
 #include "ternary.h"
+
+long int *r_ternary( long int *first, long int *last, long int value, long int *counter )
+{
+	(*counter) ++;
+	if(first == last){
+		return last;
+	}
+	
+	long int *last_backup = last;
+	long int *left = first + ( first - last )/(long int)3;
+	long int *right = left + ( first - last )/(long int)3;
+	
+	//for the left pointer
+	if(*left == value){ //*left is the value
+		return left; 	
+	}else{ //if *left != value 
+		if(*left > value){
+			last = left;
+				return r_ternary( first, last, value, counter );
+		}else{ //if *left < value
+			first = left + 1;
+				return r_ternary( first, last, value, counter );
+		}
+
+	//for the right pointer
+	if(*right == value){ //*right is the value
+		return right;
+	}else{ //if *right != value
+		if(*right > value){ // value < *right
+			last = right;
+				return r_ternary( first, last, value, counter );
+		}else{ //if value > *right
+			first = right;
+				return r_ternary( first, last, value, counter );
+		}
+	return last_backup;	
+}
+
